2969137050f576e761d8ca3c03d703e71d7713c2
Hazem Omar <hazematwwa@gmail.com>
2025-02-10 18:23:11 +0200

    seven edition'

--- scripts/Bronz/DDL.sql
+++ scripts/Bronz/DDL.sql
@@ -7,12 +7,12 @@
     Tracbility and debugenig
 */
 
-
-CREATE OR ALTER PROCEDURE sp_crmcust_info AS
+--CODE (DATA INGEST) --
+CREATE OR ALTER PROCEDURE Bronz.sp_crmcust_info AS
 BEGIN
 -- In this case in bronz layer Extract data (Full Loaded)
 -- Extract Method (Pull extraction)
--- Extract Type (Full loaded) ---> TRuNCATE & INSERT
+-- Extract Type (Full loaded) ---> TRUNCATE & INSERT
     IF OBJECT_ID('Bronz.crm_cust_info', 'U') IS NOT NULL
         DROP TABLE Bronz.crm_cust_info;
     CREATE TABLE Bronz.crm_cust_info
@@ -25,12 +25,13 @@ BEGIN
         cst_gndr CHAR(5),
         cst_create_date DATE
     );
+
 DECLARE @set_begin_time DATETIME ;
 DECLARE @set_end_time DATETIME ;
 BEGIN TRY
     SET @set_begin_time = GETDATE();
     PRINT '======================================='
-    PRINT 'LOADING THE DATE FROM Customer_CRM'
+    PRINT 'LOADING THE DATE FROM Customer_INFO_CRM'
     PRINT 'LOADING THE DATA NOW BEFORE TRUNCATE THE TABLE' + CAST(@set_begin_time AS NVARCHAR)
     TRUNCATE TABLE Bronz.crm_cust_info;
     BULK INSERT Bronz.crm_cust_info 
@@ -43,17 +44,46 @@ BEGIN TRY
     );
     SET @set_end_time = GETDATE()
 
-    PRINT 'The Time Difference between the date before and after full inseration' + CAST(DATEDIFF(SECOND ,@set_begin_time,@set_end_time) AS NVARCHAR) 
+    PRINT 'The Time Differene between the date before and after full inseration' + CAST(DATEDIFF(SECOND ,@set_begin_time,@set_end_time) AS NVARCHAR) 
+
+    -- CREATE TABLE IN BRONZ SCHEMA (Bronz.crm_prd_info) --
+    PRINT 'NOW LOADED THE DATA IN BRONZ.crm_prd_info' + 'AND THE TIME NOW BEFORE CREATED THIS TABLE ' ;
+    SET @set_begin_time = GETDATE();
+    IF OBJECT_ID('Bronz.crm_prd_info','U') IS NOT NULL 
+        DROP TABLE Bronz.crm_prd_info;
+    CREATE TABLE Bronz.crm_prd_info
+    (
+        prd_id INT DEFAULT NULL ,
+        prd_key NVARCHAR(30) ,
+        prd_nm NVARCHAR(50) ,
+        prd_cost NVARCHAR(30) ,
+        prd_line NVARCHAR(30),
+        prd_start_dt DATETIME2,
+        prd_end_dt DATETIME2
+    );
+
+    BULK INSERT Bronz.crm_prd_info
+    FROM '/usr/database/datasets/source_crm/prd_info.csv'
+    WITH
+    (
+        FIRSTROW = 2 ,
+        FIELDTERMINATOR = ',',
+        TABLOCK  
+    );
+    SET @set_end_time = GETDATE()
+    PRINT 'The Time Differene between the date before and after full inseration' + CAST(DATEDIFF(SECOND ,@set_begin_time,@set_end_time) AS NVARCHAR) 
 END TRY
 BEGIN CATCH
     PRINT 'ERROR OCCURED IN MY FINCTION '
     PRINT CONCAT('ERROR MESSAGE ' , CAST(ERROR_MESSAGE() AS VARCHAR));
     PRINT CONCAT('ERROR MESSAGE ' , CAST(ERROR_NUMBER() AS VARCHAR));
 END CATCH
-END;
+END
 
-EXEC sp_crmcust_info ;
+EXEC Bronz.sp_crmcust_info ;
+SELECT * FROM Bronz.crm_prd_info;
+DROP PROCEDURE Bronz.sp_crmcust_info;
 
-DROP PROCEDURE sp_crmcust_info;
 
-SELECT * FROM Bronz.crm_cust_info;
+
+
